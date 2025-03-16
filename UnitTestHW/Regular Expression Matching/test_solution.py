6fe90a4b65876b7e0f45ac9779b7ffe7b1a6490d
Lanren <lanren.tw@gmail.com>
2025-03-16 16:55:35 +0800

    fix: fix test_solution.py for Regular Expression Matching

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -18,7 +18,7 @@ testcases = [
 
 @pytest.mark.parametrize(["s", "p", "expected_res"], testcases)
 def test_is_match(solution, s, p, expected_res):
-    assert solution.isMatch(s, p) == expected
+    assert solution.isMatch(s, p) == expected_res
 
 @pytest.mark.xfail
 def test_broken_solution(solution):
